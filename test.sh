8d8853bfaf72a2d3a3649b314b2afbf9cbf1fddb
abidkyo <kyotasuna95@gmail.com>
2022-01-30 18:17:37 +0100

    ENH improve test script
    Remove test exit code for the first 3 test
    because it is not how it works.
    
    Exit script if test exit code failed.

--- test.sh
+++ test.sh
@@ -3,22 +3,6 @@
 # Author: Abid Sulaiman
 # Title: Test Script for Sound Control
 
-test_exit_code_success() {
-	if [[ $? -eq 0 ]]; then
-		echo "Exit code set correctly"
-	else
-		echo "Exit code set incorrectly"
-	fi
-}
-
-test_exit_code_fail() {
-	if [[ $? -ne 0 ]]; then
-		echo "Exit code set correctly"
-	else
-		echo "Exit code set incorrectly"
-	fi
-}
-
 # Test Output Result ------------------------------------
 
 # Test Get Input
@@ -27,30 +11,22 @@ res=$(./sound-ctl.sh gi)
 
 if [[ "$res" =~ $re ]]; then
 	echo "Test get input status OK"
-	test_exit_code_success
 else
 	echo "Test get input status failed."
 	echo "Consider running setup script or check the inputSource config."
-	test_exit_code_fail
 	exit 1
 fi
-
-echo # blank line
 
 # Test Get Output
 re='^H?S?P$' # HP or SP
 res=$(./sound-ctl.sh go)
 if [[ "$res" =~ $re ]]; then
 	echo "Test get output status OK"
-	test_exit_code_success
 else
 	echo "Test get output status failed."
 	echo "Consider running setup script or check the outputSink, speakerPort and headphonePort config."
 	exit 1
-	test_exit_code_fail
 fi
-
-echo # blank line
 
 # Test Get Volume
 re='^1?[0-9]{2}' # on or off
@@ -58,15 +34,35 @@ res=$(./sound-ctl.sh gv)
 
 if [[ "$res" =~ $re ]]; then
 	echo "Test get volume OK"
-	test_exit_code_success
 else
 	echo "Test get volume failed."
 	echo "Consider running setup script or check the outputSink config."
-	test_exit_code_fail
 	exit 1
 fi
 
 echo # blank line
+
+# Test Exit Code Function -------------------------------
+
+test_exit_code_success() {
+	if [[ $? -eq 0 ]]; then
+		echo "Exit code set correctly"
+	else
+		echo "Exit code set incorrectly"
+		return 1
+	fi
+	return 0
+}
+
+test_exit_code_fail() {
+	if [[ $? -ne 0 ]]; then
+		echo "Exit code set correctly"
+	else
+		echo "Exit code set incorrectly"
+		return 1
+	fi
+	return 0
+}
 
 # Test Command ------------------------------------------
 # with correct config -----------------------------------
@@ -78,7 +74,7 @@ echo # blank line
 for o in "${options[@]}"; do
 	echo "Test option" "$o"
 	./sound-ctl.sh $o
-	test_exit_code_success
+	test_exit_code_success || exit 1
 	echo # blank line
 done
 
@@ -94,7 +90,7 @@ echo # blank line
 for o in "${options[@]}"; do
 	echo "Test option" "$o"
 	./sound-ctl-error.sh $o
-	test_exit_code_fail
+	test_exit_code_fail || exit 1
 	echo # blank line
 done
 
